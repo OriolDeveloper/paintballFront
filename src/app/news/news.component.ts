d7afff270f75e2e5b44ac3bcadef9d9b78f0295e
OriolProgramacion <orioltrabajosprogramacion@gmail.com>
2025-11-02 01:13:58 +0100

    Config FontAwesome
    Add fontAwesome icons + add buttons admin in news view

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -3,16 +3,22 @@ import { Component, OnInit } from '@angular/core';
 import { NewsService } from './service/news.service';
 import { Router, RouterModule } from '@angular/router';
 import { NewsParameters } from './model/NewsParameters';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTrash, faEdit, faPlus } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-news',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FontAwesomeModule],
   templateUrl: './news.component.html',
   styleUrl: './news.component.scss',
   standalone: true
 })
 export class NewsComponent implements OnInit {
   news: NewsParameters[] = [];
+  faTrash = faTrash;
+  faEdit = faEdit;
+  faPlus = faPlus;
 
   constructor(private newsService: NewsService, private router: Router) {
 
